b29471261313008e24125f2eca0df1e28c5c3c34
MeyBirdsong <74513901+MeyBirdsong@users.noreply.github.com>
2020-12-18 20:54:50 -0600

    resto-navbar

---
+++ src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+class HomePage extends Component {
+    render(){
+      return(
+        <div>
+          <h1>Mr.Flumdumppy Restaurant</h1>
+          <img height="175" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOJbf-JI_m1LxaE6-FFNOgu-WrAyeqPCwhJg&usqp=CAU "/>
+          <h3> Come to see us for "Amazing food" </h3>
+          <img height="275" width="500"src="https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=6&m=1155240408&s=612x612&w=0&h=SEhOUzsexrBBtRrdaLWNB6Zub65Dnyjk7vVrTk1KQSU=" />
+
+          
+        </div>
+      );
+    }
+  }
+
+  export default HomePage;
---
+++ src/components/Speciality.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+
+class Speciality extends Component {
+    render(){
+      return(
+        <div>
+          <h1>Our Speciality For Today :</h1>          
+            <ul>
+              <li>Spring Mix Salad with fried Oyster </li>
+              <li>Salmon with truffle sauce</li>
+              <li>Cheese Cake with Rasberry Chocolate</li>
+            </ul>
+          
+        </div>
+      );
+    }
+  }
+
+  export default Speciality;
+  
